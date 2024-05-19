381f8bbe7c9aa9c67358fa11fc8c0c24ac0fe17a
Jacob Javier <146559412+jpjavier23@users.noreply.github.com>
2024-05-19 00:15:11 -0400

    working on part 2

--- SQLFiles Tier 1/SQLTasks Tier 1.sql
+++ SQLFiles Tier 1/SQLTasks Tier 1.sql
@@ -34,35 +34,72 @@ exploring the data, and getting acquainted with the 3 tables. */
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Write a SQL query to produce a list of the names of the facilities that do. */
 
+SELECT name
+FROM Facilities
+WHERE membercost > 0
+LIMIT 0 , 1000;
 
 /* Q2: How many facilities do not charge a fee to members? */
 
+SELECT COUNT(name) AS cnt_name
+FROM Facilities
+WHERE membercost = 0
+LIMIT 0, 1000;
 
 /* Q3: Write an SQL query to show a list of facilities that charge a fee to members,
 where the fee is less than 20% of the facility's monthly maintenance cost.
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
 
+SELECT facid, name, membercost, monthlymaintenance
+FROM Facilities
+WHERE membercost > 0 
+	AND (membercost < monthlymaintenance * 0.2)
+LIMIT 0, 1000;
 
 /* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
 Try writing the query without using the OR operator. */
 
+SELECT *
+FROM Facilities
+WHERE name LIKE '% 2'
+LIMIT 0, 1000;
 
 /* Q5: Produce a list of facilities, with each labelled as
 'cheap' or 'expensive', depending on if their monthly maintenance cost is
 more than $100. Return the name and monthly maintenance of the facilities
 in question. */
 
+SELECT name, monthlymaintenance, 
+CASE WHEN monthlymaintenance > 100 THEN 'expensive'
+	ELSE 'cheap' END AS affordability
+FROM Facilities
+LIMIT 0, 1000;
 
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Try not to use the LIMIT clause for your solution. */
 
+SELECT firstname, surname
+FROM Members
+WHERE joindate = 
+	(SELECT max(joindate) 
+	FROM Members)
+LIMIT 0, 1000;
 
 /* Q7: Produce a list of all members who have used a tennis court.
 Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
 
+SELECT DISTINCT f.name as facility_name, CONCAT_WS(' ', m.firstname, m.surname) as name
+FROM Bookings as b,
+      Facilities as f,
+      Members as m
+WHERE b.memid = m.memid
+	AND b.facid = f.facid
+	AND f.name LIKE 'Tennis%'
+ORDER BY name
+LIMIT 0, 1000;
 
 /* Q8: Produce a list of bookings on the day of 2012-09-14 which
 will cost the member (or guest) more than $30. Remember that guests have
@@ -71,9 +108,41 @@ the guest user's ID is always 0. Include in your output the name of the
 facility, the name of the member formatted as a single column, and the cost.
 Order by descending cost, and do not use any subqueries. */
 
+SELECT f.name as facility_name, 
+	CONCAT_WS(' ', m.firstname, m.surname) as name,
+	CASE
+		WHEN m.memid = 0 THEN f.guestcost * b.slots
+		ELSE f.membercost * b.slots
+	END AS cost
+FROM Bookings as b
+JOIN Facilities as f
+ON b.facid = f.facid
+JOIN Members as m
+ON b.memid = m.memid
+WHERE b.starttime LIKE '2012-09-14%'
+ORDER BY cost DESC
+LIMIT 0, 12;
 
 /* Q9: This time, produce the same result as in Q8, but using a subquery. */
 
+SELECT (SELECT name
+        FROM Facilities
+        WHERE sub.facid = Facilities.facid) AS facility_name,
+		(SELECT CONCAT_WS(' ', firstname, surname)
+         FROM Members
+         WHERE sub.memid = Members.memid) AS name,
+		cost
+FROM (SELECT b.facid as facid,
+      b.memid as memid,
+      CASE WHEN b.memid = 0 THEN f.guestcost * b.slots
+      	ELSE f.membercost * b.slots
+      END AS cost
+    FROM Bookings as b
+    JOIN Facilities as f
+    ON b.facid = f.facid
+    WHERE b.starttime LIKE '2012-09-14%') AS sub
+ORDER BY cost DESC
+LIMIT 0, 12;
 
 /* PART 2: SQLite
 /* We now want you to jump over to a local instance of the database on your machine. 
@@ -97,6 +166,14 @@ that there's a different cost for guests and members! */
 
 /* Q11: Produce a report of members and who recommended them in alphabetic surname,firstname order */
 
+SELECT CONCAT_WS(', ', m1.surname, m1.firstname) AS name,
+	CASE
+		WHEN m2.surname LIKE 'GUEST' THEN NULL
+		ELSE CONCAT_WS(', ', m2.surname, m2.firstname) 
+	END AS referred_by
+FROM Members as m1
+INNER JOIN Members as m2
+ON m2.memid = m1.recommendedby
 
 /* Q12: Find the facilities with their usage by member, but not guests */
 
